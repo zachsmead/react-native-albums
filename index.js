f9be7fae58c02ac78ce93e5d2dc1ce9614ba63a9
= <zrsmead@gmail.com>
2018-06-20 14:11:19 -0700

    index

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
+// Create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
